0fbec607ca792c25abf1e6547a209b517fc4fa85
Rami Salhi <ramisalhi@Ramis-MacBook-Pro.local>
2015-11-21 20:29:18 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Cup-space</title>
+	<style type="text/css">
+		.container {
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<h1>CUP-SPACE.COM</h1>
+		<br>
+		<br>
+		<h2>SIMPLY CREATING ADVERTISING SPACE</h2>
+		<h3>For enquires email: <a href="mailto:cup-space@hotmail.com?Subject=Enquiry" target="_top">cup-space@hotmail.com</a></h3>
+	</div>
+</body>
+</html>
